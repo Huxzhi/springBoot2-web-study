f8d3791e5f7d09de4d6605e29033ee25ab8bcee9
Huxzhi <huxzhi@gmail.com>
2022-01-15 19:42:03 +0800

    测试@PathVariable、@RequestHeader、@ModelAttribute、@RequestParam、@MatrixVariable、@CookieValue、@RequestBody

---
+++ boot-05-web-01/src/main/java/com/atguigu/boot/controller/HelloController.java
@@ -0,0 +1,48 @@
+package com.atguigu.boot.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloController {
+    //    @RequestMapping(value = "/user",method = RequestMethod.GET)
+    @GetMapping("/user")
+    public String getUser() {
+        return "GET-张三";
+    }
+
+    //    @RequestMapping(value = "/user",method = RequestMethod.POST)
+    @PostMapping("/user")
+    public String saveUser() {
+        return "POST-张三";
+    }
+
+
+    //    @RequestMapping(value = "/user",method = RequestMethod.PUT)
+    @PutMapping("/user")
+    public String putUser() {
+        return "PUT-张三";
+    }
+
+    //    @RequestMapping(value = "/user",method = RequestMethod.DELETE)
+    @DeleteMapping("/user")
+    public String deleteUser() {
+        return "DELETE-张三";
+    }
+
+
+//    @Bean
+//    @ConditionalOnMissingBean(HiddenHttpMethodFilter.class)
+//    @ConditionalOnProperty(prefix = "spring.mvc.hiddenmethod.filter", name = "enabled", matchIfMissing = false)
+//    public OrderedHiddenHttpMethodFilter hiddenHttpMethodFilter() {
+//        return new OrderedHiddenHttpMethodFilter();
+//    }
+//
+//
+//    //自定义filter
+//    @Bean
+//    public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
+//        HiddenHttpMethodFilter methodFilter = new HiddenHttpMethodFilter();
+//        methodFilter.setMethodParam("_m");
+//        return methodFilter;
+//    }
+}
